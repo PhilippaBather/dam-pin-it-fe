160a80d66c1fddf8eb7b142543aa26afd15389c3
pabather <p.a.bather1@gmail.com>
2024-10-24 00:06:26 +0200

    added error msg constants

---
+++ src/constants/error-messages.js
@@ -0,0 +1,11 @@
+export const errFirstnameRequired = "A name is required";
+export const errorFirstnameInvalid = "A valid name is required";
+export const errorSurnameRequired = "A surname is required";
+export const errorSurnameInvalid = "A valid surname is required";
+export const errorEmailReq = "Please enter your email";
+export const errorEmailInv = "Please enter a valid email address.";
+export const errorEmailConfirmation =
+  "Email does not match. Please re-enter your email.";
+export const errorPasswordRequired = "Please enter your password.";
+export const errorPasswordInv =
+  "Password must be between 8 to 25 characters containing at least 1 capital letter, a number, and a special character.";
