63ea5985890dae688ff60c04eb3663dca3ecb24a
pabather <p.a.bather1@gmail.com>
2024-10-25 00:02:17 +0200

    updated routing and added auth loaders

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,30 +3,32 @@ import ErrorPage from "../pages/public/ErrorPage";
 import HomePage from "../pages/public/HomePage";
 import LoginPage from "../pages/public/LoginPage";
 import RegistrationPage from "../pages/public/RegistrationPage";
-import PrivateRoutes from "../pages/routes/PrivateRoutes";
 import ProjectHomePage from "../pages/private/ProjectHomePage";
 import RootLayout from "../pages/public/RootLayout";
 import ROUTES from "../pages/routes/routes";
+import { action as logoutAction } from "../pages/private/Logout.js";
+import { checkAuthLoader, tokenLoader } from "../auth/auth-functions.js";
 
-//every object reflects a route
 const router = createBrowserRouter([
   {
     path: ROUTES.HOME,
-    element: <PrivateRoutes />,
-    errorElement: <ErrorPage />,
-    children: [
-      { path: ROUTES.PROJECT_HOME_PAGE, element: <ProjectHomePage /> },
-    ],
-  },
-
-  {
-    path: "/",
+    id: "root",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
+    loader: tokenLoader,
     children: [
+      {
+        path: ROUTES.PROJECT_HOME_PAGE,
+        element: <ProjectHomePage />,
+        loader: checkAuthLoader,
+      },
       { index: true, element: <HomePage /> },
       { path: ROUTES.LOGIN, element: <LoginPage /> },
       { path: ROUTES.REGISTER, element: <RegistrationPage /> },
+      {
+        path: "/logout",
+        action: logoutAction,
+      },
     ],
   },
 ]);
