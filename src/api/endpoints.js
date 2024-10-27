0b5bc2c5bdd3dc67959b4bd8b4c662e63348e286
pabather <p.a.bather1@gmail.com>
2024-10-27 14:51:29 +0100

    added endpoing constants for auth and origin

---
+++ src/api/endpoints.js
@@ -0,0 +1,4 @@
+export const origin = "http://127.0.0.1:5173";
+
+export const loginEndpoint = "http://localhost:3000/users/auth/login";
+export const signupEndpoint = "http://localhost:3000/users/auth/signup";
