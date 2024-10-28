a7c600b74aeaf331f460952ae646aeaa9decfed8
pabather <p.a.bather1@gmail.com>
2024-10-28 21:48:46 +0100

    added logout route

--- src/pages/routes/routes.js
+++ src/pages/routes/routes.js
@@ -2,6 +2,7 @@ export default Object.freeze({
   HOME: "/",
   ERROR: "/error",
   LOGIN: "/login",
+  LOGOUT: "/logout",
   REGISTER: "/register",
   PASSWORD_RECOVERY: "/password-recovery",
   PROJECT_HOME_PAGE: "/projects-home",
