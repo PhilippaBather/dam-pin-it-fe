85635f68e718f64980f35c47ea57269ad5e0e05d
pabather <p.a.bather1@gmail.com>
2024-11-11 02:19:57 +0100

    updated to handle task dnd

--- src/context/project-context.jsx
+++ src/context/project-context.jsx
@@ -3,18 +3,49 @@ import { createContext, useContext, useState } from "react";
 // create context
 const ProjectContext = createContext({
   projects: [],
+  tasks: {},
 });
 
 // create provider
 export const ProjectContextProvider = ({ children }) => {
   const [projectsState, setProjectsState] = useState([]);
   const [currProject, setCurrProject] = useState({});
+  const [tasks, setTasks] = useState({});
+  const [selectedTask, setSelectedTask] = useState({});
+
+  const updateDraggedTasksXAxis = (updatedTasks) => {
+    setTasks((prev) => {
+      return {
+        ...prev,
+        ...updatedTasks,
+      };
+    });
+  };
+
+  const updateDraggedTasksYAxis = (updatedStartTasks, updatedEndTasks) => {
+    setTasks((prev) => {
+      return {
+        ...prev,
+        ...updatedStartTasks,
+        ...updatedEndTasks,
+      };
+    });
+  };
 
   const ctxtValue = {
     currProject,
     projects: projectsState,
+    projectTasks: tasks,
+    selectedTask,
     setCurrProject: setCurrProject,
     setProjects: setProjectsState,
+    setTasks,
+    setSelectedTask,
+    updateDraggedTasksXAxis,
+    updateDraggedTasksYAxis,
+
+    // updateChangedColumn,
+    // updateReorderedColumn,
   };
 
   return (
