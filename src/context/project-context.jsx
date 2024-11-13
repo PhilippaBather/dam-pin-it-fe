9d3282c46410b7de05399fbb9be68a4cc5a973c5
pabather <p.a.bather1@gmail.com>
2024-11-14 00:10:38 +0100

    added to handle tasks; placeholder data added for testing

--- src/context/project-context.jsx
+++ src/context/project-context.jsx
@@ -10,32 +10,110 @@ const ProjectContext = createContext({
 export const ProjectContextProvider = ({ children }) => {
   const [projectsState, setProjectsState] = useState([]);
   const [currProject, setCurrProject] = useState({});
-  const [tasks, setTasks] = useState({});
+  const [tasks, setTasks] = useState({
+    1: {
+      tasks: [
+        {
+          id: 1,
+          projectId: "1",
+          title: "Task 1",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "PENDING",
+          position: 1,
+        },
+        {
+          id: 2,
+          projectId: "1",
+          title: "Task 2",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "PENDING",
+          position: 2,
+        },
+        {
+          id: 6,
+          projectId: "1",
+          title: "Task 6",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "PENDING",
+          position: 3,
+        },
+      ],
+    },
+    2: {
+      tasks: [
+        {
+          id: 4,
+          projectId: "1",
+          title: "Task 4",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "IN_PROGRESS",
+          position: 1,
+        },
+        {
+          id: 3,
+          projectId: "1",
+          title: "Task 3",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "IN_PROGRESS",
+          position: 2,
+        },
+      ],
+    },
+    3: {
+      tasks: [],
+    },
+    4: {
+      tasks: [
+        {
+          id: 5,
+          projectId: "1",
+          title: "Task 5",
+          description: "taskDescription",
+          deadline: "01/01/2025",
+          priority: "NORMAL",
+          status: "COMPLETED",
+          position: 1,
+        },
+      ],
+    },
+  });
   const [selectedTask, setSelectedTask] = useState({});
 
-  const updateDraggedTasksXAxis = (updatedTasks) => {
+  const updateDraggedTasksYAxis = (colId, updatedTasks) => {
     setTasks((prev) => {
       return {
         ...prev,
-        ...updatedTasks,
+        [colId]: {
+          tasks: updatedTasks,
+        },
       };
     });
   };
 
-  const updateDraggedTasksYAxis = (
-    startColId,
-    updatedStartTasks,
-    endColId,
-    updatedEndTasks
+  const updateDraggedTasksXAxis = (
+    sourceColId,
+    destinationColId,
+    updatedSourceTasks,
+    updatedDestinationTasks
   ) => {
     setTasks((prev) => {
       return {
         ...prev,
-        [startColId]: {
-          tasks: [...updatedStartTasks],
+        [sourceColId]: {
+          tasks: [...updatedSourceTasks],
         },
-        [endColId]: {
-          tasks: [...updatedEndTasks],
+        [destinationColId]: {
+          tasks: [...updatedDestinationTasks],
         },
       };
     });
