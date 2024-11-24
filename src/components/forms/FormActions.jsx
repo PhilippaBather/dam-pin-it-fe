a7b5f25594f29363944195d29c0eac391bd32822
pabather <p.a.bather1@gmail.com>
2024-11-24 17:16:29 +0100

    refactored

--- src/components/forms/FormActions.jsx
+++ src/components/forms/FormActions.jsx
@@ -1,12 +1,30 @@
-function FormActions({ btnLabel1, btnLabel2, handleClick }) {
+import { Link } from "react-router-dom";
+
+function FormActions({
+  btnLabel1,
+  btnLabel2,
+  handleClick,
+  isLink = false,
+  link = null,
+  btn2Type = "button",
+}) {
   return (
     <div className="form-btn__container">
       <button className="form-btn" type="submit">
         {btnLabel1}
       </button>
-      <button className="form-btn" type="reset" onClick={handleClick}>
-        {btnLabel2}
-      </button>
+      {!isLink && (
+        <button className="form-btn" type={btn2Type} onClick={handleClick}>
+          {btnLabel2}
+        </button>
+      )}
+      {isLink && (
+        <button className="form-btn" type={btn2Type} onClick={handleClick}>
+          <Link to={link} className="form-btn_link">
+            {btnLabel2}
+          </Link>
+        </button>
+      )}
     </div>
   );
 }
