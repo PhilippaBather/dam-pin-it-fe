150b38b7210f022e3a19e21a6a64bb09df563ea8
pabather <p.a.bather1@gmail.com>
2024-11-24 17:16:50 +0100

    updated

--- src/pages/public/LoginPage.jsx
+++ src/pages/public/LoginPage.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import Card from "../../components/ui/Card";
+import FormActions from "../../components/forms/FormActions.jsx";
 import { handleHttpReq, postAuthData } from "../../api/http-requests.js";
 import {
   errorEmailReq as emailReq,
@@ -10,6 +11,7 @@ import {
 import "../../stylesheets/form.css";
 import "../../stylesheets/ui-components.css";
 import { loginEndpoint, userDataEndpoint } from "../../api/endpoints.js";
+import ROUTES from "../routes/routes.js";
 
 function LoginPage() {
   const {
@@ -68,14 +70,15 @@ function LoginPage() {
             {passReq}
           </span>
         )}
-        <div className="form-btn__container">
-          <button className="form-btn" type="submit">
-            Login
-          </button>
-          <button className="form-btn" type="button">
-            Cancel
-          </button>
-        </div>
+        <FormActions
+          btnLabel1={"Login"}
+          btnLabel2={"Cancel"}
+          isLink={true}
+          link={ROUTES.HOME}
+        />
+        <Link to={ROUTES.PASSWORD_RECOVERY} className="form-link">
+          Forgotten your password?
+        </Link>
       </form>
     </Card>
   );
