060dc76cbd2f0a30a002483956b3dd6db84eddf6
pabather <p.a.bather1@gmail.com>
2024-11-24 11:05:38 +0100

    updated

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -1,5 +1,17 @@
+import "../../stylesheets/home-page.css";
+
 function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <main className="backdrop-home">
+      <h1>¡Pin-it App!</h1>
+      <h2>Cards, Columns, Clarity | Flow, Focus, Finish</h2>
+      <h3>
+        An intuitive project management solution for a diverse range of needs:
+        from personal projects to multi-person collaborations.
+      </h3>
+      <h4>Register for Free</h4>
+    </main>
+  );
 }
 
 export default HomePage;
