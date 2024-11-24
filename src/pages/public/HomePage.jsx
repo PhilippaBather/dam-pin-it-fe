e83af1703c050bf525e81e508ca131ba57254f13
pabather <p.a.bather1@gmail.com>
2024-11-24 15:42:41 +0100

    added home page text

--- src/pages/public/HomePage.jsx
+++ src/pages/public/HomePage.jsx
@@ -3,13 +3,15 @@ import "../../stylesheets/home-page.css";
 function HomePage() {
   return (
     <main className="backdrop-home">
-      <h1>¡Pin-it App!</h1>
-      <h2>Cards, Columns, Clarity | Flow, Focus, Finish</h2>
-      <h3>
-        An intuitive project management solution for a diverse range of needs:
-        from personal projects to multi-person collaborations.
+      <h1 className="app-title">¡Pin-it App!</h1>
+      <h2 className="app-slogan">
+        Cards, Columns, Clarity | Flow, Focus, Finish
+      </h2>
+      <h3 className="app-description">
+        An intuitive project management solution for personal projects and
+        multi-person collaborations.
       </h3>
-      <h4>Register for Free</h4>
+      <h4 className="app-reg">Register for Free</h4>
     </main>
   );
 }
