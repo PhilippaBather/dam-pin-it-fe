e0659961ac3605b001efae7a486d7e182885ab78
pabather <p.a.bather1@gmail.com>
2024-12-02 18:07:53 +0100

    added fetch call and mapped list

--- src/components/ui/TableShared.jsx
+++ src/components/ui/TableShared.jsx
@@ -1,40 +1,77 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import Card from "./Card";
+import { useProjectContext } from "../../context/project-context";
+import { getAuthToken } from "../../auth/auth-functions";
 import "../../stylesheets/table.css";
 
 function TableShared() {
+  const { id } = useParams();
+  const { sharedProjects, setSharedProjects } = useProjectContext();
+  const handleLeaveSharedProject = (projectId) => {
+    console.log(projectId);
+  };
+
+  useEffect(() => {
+    const token = getAuthToken();
+    const getSharedProjects = async () => {
+      try {
+        const resp = await fetch(
+          `http://localhost:3000/guests/shared-projects/${id}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Origin: origin,
+              Authorizaton: "Bearer " + token,
+            },
+          }
+        );
+        const data = await resp.json();
+        console.log(data);
+        setSharedProjects(data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getSharedProjects();
+  }, [id, setSharedProjects]);
+
   return (
     <section className="table-shared">
       <Card isTable>
         <h1 className="table-title">Shared Projects</h1>
         <div className="table-container__head">
           <div className="table-row__head">Project</div>
+          <div className="table-row__head">Permissions</div>
           <div className="table-row__head">Deadline</div>
           <div className="table-row__head">Owner</div>
           <div className="table-row__head">Email</div>
-          <div className="table-row__head">Participants</div>
-          <div className="table-row__head">Email</div>
+          <div className="table-row__head">Remove Project</div>
         </div>
         <div className="table-container__body">
-          <div className="table-row__body">Christmasdddddddddddddddd</div>
-          <div className="table-row__body">01/01/2025</div>
-          <div className="table-row__body">Molly Chops</div>
-          <div className="table-row__body">molly@chops.com</div>
-          <div className="table-row__body">
-            <ul className="table-row__body-list">
-              <li className="table-row__body-guests">Joe Bloggs</li>
-              <li className="table-row__body-guests">Snoopy</li>
-              <li className="table-row__body-guests">Dennis the Menace</li>
-              <li className="table-row__body-guests">Doraemon</li>
-            </ul>
-          </div>
-          <div className="table-row__body">
-            <ul className="table-row__body-list">
-              <li className="table-row__body-guests">test@test.com</li>
-              <li className="table-row__body-guests">test@test.com</li>
-              <li className="table-row__body-guests">test@test.com</li>
-              <li className="table-row__body-guests">test@test.com</li>
-            </ul>
-          </div>
+          {sharedProjects &&
+            sharedProjects?.map((project) => (
+              <>
+                <div className="table-row__body">{project.projectTitle}</div>
+                <div className="table-row__body">
+                  {project.permissions.toLowerCase()}
+                </div>
+                <div className="table-row__body">{project.deadline}</div>
+                <div className="table-row__body">{`${project.ownerName} ${project.ownerSurname}`}</div>
+                <div className="table-row__body">{project.ownerEmail}</div>
+                <div className="table-row__body">
+                  {" "}
+                  <button
+                    className="card-btn table-btn"
+                    type="button"
+                    onClick={() => handleLeaveSharedProject(project.projectId)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              </>
+            ))}
         </div>
       </Card>
     </section>
