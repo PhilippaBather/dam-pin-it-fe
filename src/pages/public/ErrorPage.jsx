69648fe174b74577e0a3d34b16daa2d1adfa5276
pabather <p.a.bather1@gmail.com>
2024-12-08 01:05:07 +0100

    handled error page redirect and logout logic

--- src/pages/public/ErrorPage.jsx
+++ src/pages/public/ErrorPage.jsx
@@ -1,18 +1,27 @@
 import { Link } from "react-router-dom";
-import MainNavigation from "../../components/navigation/MainNavigation";
 import Card from "../../components/ui/Card";
+import MainNavigation from "../../components/navigation/MainNavigation";
+import { useUIContext } from "../../context/ui-context";
+import { logout } from "../private/Logout";
 import "../../stylesheets/error-page.css";
 
 function ErrorPage() {
+  const { globalError } = useUIContext();
+
+  if (globalError) {
+    logout();
+  }
+
   return (
     <>
       <MainNavigation />
       <main className="background-error">
         <Card>
           <h1 className="error-title">¡An error occurred!</h1>
+          <p className="error-para">{globalError}</p>
           <p className="error-para__link">
-            <Link to="/" className={"error-link"}>
-              Go to home page
+            <Link to="/login" className={"error-link"}>
+              Login
             </Link>
           </p>
           <p className="background-attribution">
