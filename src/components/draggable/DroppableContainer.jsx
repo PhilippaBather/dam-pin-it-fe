4f117b33ede742425addf0b0500a913b33e54905
pabather <p.a.bather1@gmail.com>
2024-12-08 10:06:44 +0100

    updated err, loading, http for draggables

--- src/components/draggable/DroppableContainer.jsx
+++ src/components/draggable/DroppableContainer.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { Droppable } from "react-beautiful-dnd";
 import ListContainer from "./ListContainer";
+import LoadingDots from "../ui/spinners/LoadingDots";
 import TaskContainer from "./TaskContainer";
+import { useProjectContext } from "../../context/project-context";
 
-function DroppableContainer({ column, tasks }) {
+function DroppableContainer({ column, tasks, isLoading, httpError }) {
   const [droppableEnabled, setDroppableEnabled] = useState(false);
+  const { currProject } = useProjectContext();
 
   /**
    * Workaround  to use react-bautiful-dnd with StrictMode in React 18.
@@ -23,20 +26,25 @@ function DroppableContainer({ column, tasks }) {
 
   return (
     <ListContainer colId={column} className={`list-container column`}>
-      <Droppable droppableId={column}>
-        {(provided) => (
-          <ul className="list-container__list" ref={provided.innerRef}>
-            {tasks?.map((task) => (
-              <TaskContainer
-                index={task.taskPosition}
-                task={task}
-                key={task.id}
-              />
-            ))}
-            {provided.placeholder}
-          </ul>
-        )}
-      </Droppable>
+      {isLoading && !currProject && (
+        <LoadingDots dotColor="rgba(251, 5, 173, 0.7)" />
+      )}
+      {!httpError && (
+        <Droppable droppableId={column}>
+          {(provided) => (
+            <ul className="list-container__list" ref={provided.innerRef}>
+              {tasks?.map((task) => (
+                <TaskContainer
+                  index={task.taskPosition}
+                  task={task}
+                  key={task.id}
+                />
+              ))}
+              {provided.placeholder}
+            </ul>
+          )}
+        </Droppable>
+      )}
     </ListContainer>
   );
 }
